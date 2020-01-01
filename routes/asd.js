e7137a555bdeaaf88cde52006e1a4ac6ad5e690b
Dahmane Seghier <worw787@gmail.com>
2020-01-01 13:51:34 +0100

    Logging error on /asd route

--- routes/asd.js
+++ routes/asd.js
@@ -24,6 +24,7 @@ module.exports = async (req, res, next) => {
         });
         next();
     } catch (error) {
-        return next(new errors.InternalError('Error:002 '));
+        console.error(error)
+        return next(new errors.InternalError('Error:002'));
     }
 };
